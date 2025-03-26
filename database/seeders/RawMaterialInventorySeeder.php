1329981b96c688d286b30ff900e7f148229a8779
LuisFel123 <felipe_ramirez1234@hotmail.com>
2025-03-26 01:54:22 -0600

    Cambios de seeder

--- database/seeders/RawMaterialInventorySeeder.php
+++ database/seeders/RawMaterialInventorySeeder.php
@@ -17,7 +17,7 @@ class RawMaterialInventorySeeder extends Seeder
         for ($i = 0; $i < 100; $i++) { // Generar 10 registros aleatorios
             DB::table('raw_material_inventories')->insert([
                 'stockIdeal' =>round(mt_rand(2000, 5000) / 100, 4), // Diametro entre 20.00 y 50.00 // Cantidad entre 5 y 5
-                'idUsuario' =>2
+                'idUsuario' =>1
             ]);
         }
     }
